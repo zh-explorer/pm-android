37cdabc8e2a1ae7c46d685bd7175f10d27d61361
zh_explorer <hsadkhk@gmail.com>
2015-11-25 01:43:36 +0800

    add location

--- app/src/main/java/com/example/explorer/en/MainActivity.java
+++ app/src/main/java/com/example/explorer/en/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.explorer.en;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +11,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import static com.example.explorer.en.decodeJson.decode;
 
@@ -18,6 +25,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView responseText;
 
+    private TextView locationText;
+
+    private LocationManager locationManager;
+
+    private String provider;
     public Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             switch (msg.what) {
@@ -39,8 +51,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         sendRequest = (Button) findViewById(R.id.send_response);
         responseText = (TextView) findViewById(R.id.response_text);
+        locationText = (TextView) findViewById(R.id.location);
 
         sendRequest.setOnClickListener(this);
+
+        locationInit();
+
+    }
+
+    private void locationInit() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            Toast.makeText(this, "No location provider to user", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            showLocation(location);
+        }
+
+        locationManager.requestLocationUpdates(provider, 5000, 1, locationListenter);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListenter);
+        }
+    }
+
+    LocationListener locationListenter = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void showLocation(Location location) {
+        String currentPosition = "latitude is " + location.getLatitude() + "\n"
+                + "longitude is " + location.getLongitude();
+        locationText.setText(currentPosition);
     }
 
     @Override
