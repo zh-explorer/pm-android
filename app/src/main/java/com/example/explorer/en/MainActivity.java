1dee872697d4bef52d0ca5964e9dab594e5008fc
zh_explorer <hsadkhk@gmail.com>
2015-11-25 00:44:00 +0800

    test

--- app/src/main/java/com/example/explorer/en/MainActivity.java
+++ app/src/main/java/com/example/explorer/en/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     responseText.setText(str);
             }
         }
-    };
+    }  ;
 
 
     @Override
